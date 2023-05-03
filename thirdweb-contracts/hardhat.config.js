f6f415efc55e88ccb47c5a3a50c903a08526209e
Rakshit Gupta <rakshitgupta621@gmail.com>
2023-05-03 22:13:16 +0530

    changing from goerli to sepolia

--- thirdweb-contracts/hardhat.config.js
+++ thirdweb-contracts/hardhat.config.js
@@ -2,11 +2,11 @@
 module.exports = {
   solidity: {
     version: '0.8.9',
-    defaultNetwork: 'goerli',
+    defaultNetwork: 'sepolia',
     networks: {
       hardhat: {},
-      goerli: {
-        url: 'https://rpc.ankr.com/eth_goerli',
+      sepolia: {
+        url: 'https://rpc.sepolia.org',
         accounts: [`0x${process.env.PRIVATE_KEY}`]
       },
     },
