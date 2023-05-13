1878c865b9a4baed3476ab4681a48e18d60484e3
Rakshit Gupta <rakshitgupta621@gmail.com>
2023-05-13 11:55:51 +0530

    changes in pages and context

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -1,20 +1,122 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Sepolia } from "@thirdweb-dev/chains";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
-import App from "./App";
-import { StateContextProvider } from './context';
-import "./index.css";
+import React, { useContext, createContext } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+import {
+  useAddress,
+  useContract,
+  useMetamask,
+  useContractWrite,
+  useDisconnect,
+} from "@thirdweb-dev/react";
+import { ethers } from "ethers";
+import { EditionMetadataWithOwnerOutputSchema } from "@thirdweb-dev/sdk";
 
-root.render(
-  <ThirdwebProvider activeChain={Sepolia}>
-    <Router>
-    <StateContextProvider>
-      <App />
-      </StateContextProvider>
-    </Router>
-  </ThirdwebProvider>
-);
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+  const [searchFilterWord, setSearchFilterWord] = React.useState("");
+
+  const { contract } = useContract(
+    "0x87eB2796aD2cEfc6a8a2111D95C65683a8554Fb2"
+  );
+
+  const { mutateAsync: createCampaign } = useContractWrite(
+    contract,
+    "createCampaign"
+  );
+
+  const address = useAddress();
+  const connect = useMetamask();
+  const disconnect = useDisconnect();
+
+  const publishCampaign = async (form) => {
+    try {
+      const data = await createCampaign([
+        address, // owner
+        form.title, // title
+        form.description, // description
+        form.target,
+        new Date(form.deadline).getTime(), // deadline,
+        form.image,
+      ]);
+
+      console.log("contract call success", data);
+    } catch (error) {
+      console.log("contract call failure", error);
+    }
+  };
+
+  const getCampaigns = async () => {
+    const campaigns = await contract.call("getCampaigns");
+
+    const parsedCampaings = campaigns.map((campaign, i) => ({
+      owner: campaign.owner,
+      title: campaign.title,
+      description: campaign.description,
+      target: ethers.utils.formatEther(campaign.target.toString()),
+      deadline: campaign.deadline.toNumber(),
+      amountCollected: ethers.utils.formatEther(
+        campaign.amountCollected.toString()
+      ),
+      image: campaign.image,
+      pId: i,
+    }));
+
+    return parsedCampaings;
+  };
+
+  const getUserCampaigns = async () => {
+    const allCampaigns = await getCampaigns();
+
+    const filteredCampaigns = allCampaigns.filter(
+      (campaign) => campaign.owner === address
+    );
+
+    return filteredCampaigns;
+  };
+
+  const donate = async (pId, amount) => {
+    const data = await contract.call("donateToCampaign", pId, {
+      value: ethers.utils.parseEther(amount),
+    });
+
+    return data;
+  };
+
+  const getDonations = async (pId) => {
+    const donations = await contract.call("getDonators", pId);
+    const numberOfDonations = donations[0].length;
+
+    const parsedDonations = [];
+
+    for (let i = 0; i < numberOfDonations; i++) {
+      parsedDonations.push({
+        donator: donations[0][i],
+        donation: ethers.utils.formatEther(donations[1][i].toString()),
+      });
+    }
+
+    return parsedDonations;
+  };
+
+  return (
+    <StateContext.Provider
+      value={{
+        address,
+        contract,
+        connect,
+        disconnect,
+        createCampaign: publishCampaign,
+        getCampaigns,
+        getUserCampaigns,
+        donate,
+        getDonations,
+        searchFilterWord,
+        setSearchFilterWord,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
