ec1531555c7cad9e58a6f35bc4c6950304a3b137
Rakshit Gupta <rakshitgupta621@gmail.com>
2023-05-13 11:35:06 +0530

    changing backend config

--- thirdweb-contracts/hardhat.config.js
+++ thirdweb-contracts/hardhat.config.js
@@ -6,8 +6,8 @@ module.exports = {
     networks: {
       hardhat: {},
       sepolia: {
-        url: 'https://rpc.sepolia.org',
-        accounts: [`0x${process.env.PRIVATE_KEY}`]
+        url: 'https://rpc.ankr.com/eth_sepolia',
+        accounts: [`0x${process.env.PRIVATE_KEY}`],
       },
     },
     settings: {
